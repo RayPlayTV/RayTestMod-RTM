800c09b2b1c2fafd1b621d9db9996d98fe7d60e4
RayPlayTV <RyanGerhardt2006@Gmail.com>
2023-07-20 18:24:15 +0200

    fixed Sicke Kazai Tracks

--- src/main/java/net/rayplaytv/rtm/item/ModItems.java
+++ src/main/java/net/rayplaytv/rtm/item/ModItems.java
@@ -24,27 +24,27 @@ public class ModItems {
 
     public static final RegistryObject<Item> KAZAI_MUSIC_DISC_ANGEL_NEGRO = ITEMS.register("kazai_music_disc_angel_negro",
             () -> new RecordItem(1, RTMSoundEvents.KAZAI_MUSIC_DISC_ANGEL_NEGRO_EVENT,
-                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB), 8000));
+                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB).stacksTo(1), 8000));
 
     public static final RegistryObject<Item> KAZAI_MUSIC_DISC_ASPHYXIA = ITEMS.register("kazai_music_disc_asphyxia",
             () -> new RecordItem(1, RTMSoundEvents.KAZAI_MUSIC_DISC_ASPHYXIA_EVENT,
-                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB), 8000));
+                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB).stacksTo(1), 8000));
 
     public static final RegistryObject<Item> KAZAI_MUSIC_DISC_BLOODLUST = ITEMS.register("kazai_music_disc_bloodlust",
             () -> new RecordItem(1, RTMSoundEvents.KAZAI_MUSIC_DISC_BLOODLUST_EVENT,
-                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB), 8000));
+                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB).stacksTo(1), 8000));
 
     public static final RegistryObject<Item> KAZAI_MUSIC_DISC_ITAMI = ITEMS.register("kazai_music_disc_itami",
             () -> new RecordItem(1, RTMSoundEvents.KAZAI_MUSIC_DISC_ITAMI_EVENT,
-                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB), 8000));
+                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB).stacksTo(1), 8000));
 
     public static final RegistryObject<Item> KAZAI_MUSIC_DISC_TENNO = ITEMS.register("kazai_music_disc_tenno",
             () -> new RecordItem(1, RTMSoundEvents.KAZAI_MUSIC_DISC_TENNO_EVENT,
-                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB), 8000));
+                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB).stacksTo(1), 8000));
 
     public static final RegistryObject<Item> KAZAI_MUSIC_DISC_YORU = ITEMS.register("kazai_music_disc_yoru",
             () -> new RecordItem(1, RTMSoundEvents.KAZAI_MUSIC_DISC_YORU_EVENT,
-                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB), 8000));
+                (new Item.Properties()).tab(ModCreativeModeTab.KAZ_TAB).stacksTo(1), 8000));
 
     public static final RegistryObject<Item> KAZ = ITEMS.register("kaz",
             () -> new Item(new Item.Properties()));
